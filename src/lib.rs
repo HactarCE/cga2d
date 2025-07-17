a938f22b62a1e4bb65e0b2795a19218a123129af
HactarCE <6060305+HactarCE@users.noreply.github.com>
2025-07-16 22:03:27 -0400

    Remove warnings from `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -180,20 +180,6 @@
 //! - [`Multivector`]`[`[`Axes`]`]` `->` [`Scalar`] (panics if the multivector
 //!   type doesn't support the term)
 
-#![warn(missing_docs, rust_2018_idioms)]
-#![warn(
-    clippy::cargo,
-    clippy::doc_markdown,
-    clippy::if_then_some_else_none,
-    clippy::manual_let_else,
-    clippy::semicolon_if_nothing_returned,
-    clippy::semicolon_inside_block,
-    clippy::stable_sort_primitive,
-    clippy::undocumented_unsafe_blocks,
-    clippy::uninlined_format_args,
-    clippy::unwrap_used
-)]
-
 mod axes;
 mod blade;
 mod multivector;
